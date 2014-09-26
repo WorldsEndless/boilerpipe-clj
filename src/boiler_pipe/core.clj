ae7d51036db70aa376a7fca33bbdd445244fb524
Bugra <bakyildiz@cbinsights.com>
2014-09-26 09:51:14 -0400

    Cleaning up

--- src/boiler_pipe/core.clj
+++ src/boiler_pipe/core.clj
@@ -15,11 +15,6 @@
 (defonce largest-context-extractor (LargestContentExtractor/getInstance))
 (defonce canola-extractor (CanolaExtractor/getInstance))
 
-;; Due  to version, the followings do not work (may require 1.2)
-;(defonce keep-everything-extractor (KeepEverythingExtractor/getInstance))
-;(defonce num-words-rules-extractor (NumWordsRulesExtractor/getInstance))
-;(defonce keep-everything-with-min-k-words-extractor (KeepEverythingWithMinKWordsExtractor/getInstance))
-
 (defn get-content
   "Gets either only html source of the web page, or
   web page with extractor
@@ -31,9 +26,8 @@
      (.getText extractor html)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Runs simple examples for usage of wrapper for extractors"
   [& args]
-  (println "Hello, World!")
   (def article (slurp "https://help.github.com/articles/open-source-licensing"))
   (def article (slurp "http://fivethirtyeight.com/features/lionel-messi-is-impossible/"))
   (println article)
@@ -42,7 +36,4 @@
   (println (str "\n\n\n LARGEST CONTEXT EXTRACTOR:\n\t\n" (get-content article largest-context-extractor)))
   (println (str "\n\n\n CANOLA EXTRACTOR:\n\t\n" (get-content article canola-extractor)))
 
-;  (println (str "\n\n\n KEEP EVERYTHING EXTRACTOR:\n\t\n" (get-content article keep-everything-extractor)))
-;  (println (str "\n\n\n NUM WORDS RULES EXTRACTOR:\n\t\n" (get-content article num-words-rules-extractor)))
-;  (println (str "\n\n\n KEEP EVERYTHING WITH MINIMUM K WORDS EXTRACTOR:\n\t\n" (get-content article keep-everything-with-min-k-words-extractor)))
   )

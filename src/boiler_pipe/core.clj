2bb8521d67502a8faa3ac024b40ab6b128edad6a
Bugra <bakyildiz@cbinsights.com>
2014-09-26 09:50:03 -0400

    Extra commented out extractors are added

--- src/boiler_pipe/core.clj
+++ src/boiler_pipe/core.clj
@@ -5,16 +5,20 @@
            (de.l3s.boilerpipe.extractors LargestContentExtractor)
            (de.l3s.boilerpipe.extractors CanolaExtractor)
            (de.l3s.boilerpipe.extractors KeepEverythingExtractor)
+           (de.l3s.boilerpipe.extractors KeepEverythingWithMinKWordsExtractor)
            (de.l3s.boilerpipe.extractors ExtractorBase)))
 
 ;; Available extractors in Boilerpipe
 ;; defonce prevents more than one assignment
 (defonce article-extractor (ArticleExtractor/getInstance))
 (defonce default-extractor (DefaultExtractor/getInstance))
-;(defonce largest-context-extractor (LargestContentExtractor/getInstance))
-;(defonce canola-extractor (CanolaExtractor/getInstance))
+(defonce largest-context-extractor (LargestContentExtractor/getInstance))
+(defonce canola-extractor (CanolaExtractor/getInstance))
+
+;; Due  to version, the followings do not work (may require 1.2)
 ;(defonce keep-everything-extractor (KeepEverythingExtractor/getInstance))
 ;(defonce num-words-rules-extractor (NumWordsRulesExtractor/getInstance))
+;(defonce keep-everything-with-min-k-words-extractor (KeepEverythingWithMinKWordsExtractor/getInstance))
 
 (defn get-content
   "Gets either only html source of the web page, or
@@ -22,7 +26,7 @@
   Args:
     "
   ([html]
-     (get-content html article-extractor))
+     (get-content html default-extractor))
   ([^String html ^ExtractorBase extractor]
      (.getText extractor html)))
 
@@ -31,5 +35,14 @@
   [& args]
   (println "Hello, World!")
   (def article (slurp "https://help.github.com/articles/open-source-licensing"))
+  (def article (slurp "http://fivethirtyeight.com/features/lionel-messi-is-impossible/"))
   (println article)
-  (println(get-content article)))
+  (println (str " DEFAULT EXTRACTOR:\n\t\n" (get-content article)))
+  (println (str "\n\n\n ARTICLE EXTRACTOR:\n\t\n" (get-content article article-extractor)))
+  (println (str "\n\n\n LARGEST CONTEXT EXTRACTOR:\n\t\n" (get-content article largest-context-extractor)))
+  (println (str "\n\n\n CANOLA EXTRACTOR:\n\t\n" (get-content article canola-extractor)))
+
+;  (println (str "\n\n\n KEEP EVERYTHING EXTRACTOR:\n\t\n" (get-content article keep-everything-extractor)))
+;  (println (str "\n\n\n NUM WORDS RULES EXTRACTOR:\n\t\n" (get-content article num-words-rules-extractor)))
+;  (println (str "\n\n\n KEEP EVERYTHING WITH MINIMUM K WORDS EXTRACTOR:\n\t\n" (get-content article keep-everything-with-min-k-words-extractor)))
+  )
